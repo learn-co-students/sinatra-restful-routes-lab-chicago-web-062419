6b1aaa5e4cfde053145bf004cbd2c6bb4afecfbe
github username <github email address>
2019-07-18 13:23:44 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,17 @@
-#Placeholder for a model
+
+class Recipe < ActiveRecord::Base
+
+    # RECIPES = []
+
+    # def initialize(params)
+    #     @name = params[:name]
+    #     @ingredients = params[:ingredients]
+    #     @cook_time = params[:cook_time]
+    #     RECIPES << self
+    # end 
+
+    # def self.all
+    #     RECIPES
+    # end
+
+end
